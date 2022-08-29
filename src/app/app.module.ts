0f68cf9788c432a9b06de069be296fb35dce9312
CasualSmidge <steven.midgley@maggiesottero.com>
2022-08-29 17:51:08 -0600

    Generated my first few pages and component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { AuthorizationComponent } from './pages/authorization/authorization.component';
+import { MapComponent } from './pages/map/map.component';
+import { NewsFeedComponent } from './components/news-feed/news-feed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    AuthorizationComponent,
+    MapComponent,
+    NewsFeedComponent
   ],
   imports: [
     BrowserModule,
