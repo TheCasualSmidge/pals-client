06035151dbd3d2bc77bc6b24d58656e83f0615ed
CasualSmidge <steven.midgley@maggiesottero.com>
2022-09-05 14:32:04 -0600

    Ok Ill

--- src/app/components/alert/alert.component.ts
+++ src/app/components/alert/alert.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Router, NavigationStart } from "@angular/router";
+import { Subscription } from "rxjs";
+import { Alert, AlertType } from "src/app/models";
+import { AlertService } from "src/app/services";
 
 @Component({
-  selector: 'app-alert',
-  templateUrl: './alert.component.html',
-  styleUrls: ['./alert.component.scss']
+  selector: "app-alert",
+  templateUrl: "./alert.component.html",
+  styleUrls: ["./alert.component.scss"],
 })
 export class AlertComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
