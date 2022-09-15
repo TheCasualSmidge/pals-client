a051238eff2fc5f05fce189a1b93cc5a5905e92f
CasualSmidge <steven.midgley@maggiesottero.com>
2022-09-14 18:29:25 -0600

    Added @nguniversal-express.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { PagesModule } from './pages/pages.module';
 
 @NgModule({
     imports: [
-        BrowserModule,
+        BrowserModule.withServerTransition({ appId: 'serverApp' }),
         ReactiveFormsModule,
         HttpClientModule,
         AppRoutingModule,
